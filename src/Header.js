e2ee1a6bad9d520573cfeaade13ec969d1c9a55f
Trieu Huynh <trieu.huynh@monstar-lab.com>
2021-06-08 09:57:58 +0700

    day 1

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import logo from './logo192.png';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="side-menu">
+        <img src={logo} className="avatar" alt="avatar"></img>
+        <ul className="menu-bar">
+          <li className="menu-item">
+            <a className="link">Menu 1</a>
+          </li>
+          <li className="menu-item">
+            <a className="link">Menu 2</a>
+          </li>
+          <li className="menu-item">
+            <a className="link">Menu 3</a>
+          </li>
+        </ul>
+      </div>
+      <ul className="menu-bar">
+        <li className="menu-item">
+          <a className="link">Menu 1</a>
+        </li>
+        <li className="menu-item">
+          <a className="link">Menu 2</a>
+        </li>
+        <li className="menu-item">
+          <a className="link">Menu 3</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
